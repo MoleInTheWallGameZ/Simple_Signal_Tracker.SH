9fc6b1bba304ab5318d9a0752136d09f53fb88b8
MoleInTheWallGameZ <157280874+MoleInTheWallGameZ@users.noreply.github.com>
2024-02-14 17:06:00 -0700

    Add files via upload

---
+++ War_Driveing.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+exec 3>&1 4>&2
+trap 'exec 2>&4 1>&3' 0 1 2 3
+exec 1>War_Driveing_Log.TXT 2>&1
+# Everything below will go to the file 'War_Driveing_Log.TXT':
+
+while :
+do
+    nmcli -f ALL dev wifi
+    #list all devices not just networks
+    #list all gateways/ip's
+    #List bssid
+    #list mac adress
+sleep 5
+done
